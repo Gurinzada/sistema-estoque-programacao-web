4dcbda74806a8328a5318e8645ca68dff2079d0a
Augusto <augustoinacio243@gmail.com>
2025-11-01 21:31:38 -0300

    feat: adding url for API and render page for login

--- sistema_estoque/urls.py
+++ sistema_estoque/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from estoque.views import auth_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('estoque.urls')),
+    path('', auth_view.login_page_view)
 ]
